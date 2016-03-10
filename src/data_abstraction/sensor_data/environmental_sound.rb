ba92556f5b1c96e9b9d1f68521df3d44ab2602e8
Masami Ogoshi <ogochan@wasp.co.jp>
2016-03-10 19:15:16 +0900

    new sensor

---
+++ src/data_abstraction/sensor_data/environmental_sound.rb
@@ -0,0 +1,14 @@
+module DataAbstraction::SensorData
+  class EnvironmentalSound < Generic
+    def initialize(data, meta_values = {})
+      super(data, meta_values)
+      @values = Array.new
+      data['values'].each do | value |
+        @values << SoundLevelValue.new(value.to_f, @unit)
+      end
+    end
+    def self.unit_class
+      SoundLevelValue
+    end
+  end
+end
---
+++ src/data_abstraction/sensor_data/switch.rb
@@ -0,0 +1,16 @@
+module DataAbstraction::SensorData
+  class Switch < Generic
+    def initialize(data, meta_values = {})
+      super(data, meta_values)
+      if  (( data['value'] =~ /^ON/i )
+           ( data['value'] =~ /^TRUE/i ))
+        @value = SymbolicValue.new(true)
+      else
+        @value = SymbolicValue.new(false)
+      end
+    end
+    def self.unit_class
+      SymbolicValue
+    end
+  end
+end
