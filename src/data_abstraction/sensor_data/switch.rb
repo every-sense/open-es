978b4d92c646b14b751f0c398b7d22ba2a099f6c
Masami Ogoshi <ogochan@wasp.co.jp>
2016-03-14 18:09:28 +0900

    bug patch

--- src/data_abstraction/sensor_data/switch.rb
+++ src/data_abstraction/sensor_data/switch.rb
@@ -3,7 +3,8 @@ module DataAbstraction::SensorData
     def initialize(data, meta_values = {})
       super(data, meta_values)
       if  (( data['value'] =~ /^ON/i )
-           ( data['value'] =~ /^TRUE/i ))
+           ( data['value'] =~ /^TRUE/i )
+           ( data['value'] == true ))
         @value = SymbolicValue.new(true)
       else
         @value = SymbolicValue.new(false)
